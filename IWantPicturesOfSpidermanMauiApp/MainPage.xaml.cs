0895a19d85235206ca49f82c3b73c6d490b6e42d
NaïmFarjiPXL <naim.farji@student.pxl.be>
2026-05-21 18:20:09 +0200

    I AM THE GOAT!!! Fixed detection of changes to Pictures in MainPage.cs Made changes to the layout of xaml.

--- IWantPicturesOfSpidermanMauiApp/MainPage.xaml.cs
+++ IWantPicturesOfSpidermanMauiApp/MainPage.xaml.cs
@@ -1,28 +1,31 @@
 ﻿using IWantPicturesOfSpidermanMauiApp.DTO;
 using IWantPicturesOfSpidermanMauiApp.Services;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace IWantPicturesOfSpidermanMauiApp
 {
     public partial class MainPage : ContentPage
     {
-
-        public List<PictureDTO> Pictures { get; set; } = new List<PictureDTO>();
+        public ObservableCollection<PictureDTO> Pictures { get; set; } = new();
         RestService _restService;
 
         public MainPage()
         {
             InitializeComponent();
             _restService = new RestService();
+            BindingContext = this;
         }
-
 
         private async void OnRefreshClicked(object sender, EventArgs e)
         {
             List<PictureDTO> list = new List<PictureDTO>();
             list = await _restService.GetAllPicturesAsync();
-
-            Pictures = list;
+            Pictures.Clear();
+            foreach (PictureDTO picture in list)
+            {
+                Pictures.Add(picture);
+            }
         }
     }
 }
